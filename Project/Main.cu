1a21e201a08adebd22c8a3c59cfd85bc77987ba6
Simon Blom <102825244+simonblo@users.noreply.github.com>
2023-11-16 16:02:58 +0100

    Complete exercise 1

--- Project/Main.cu
+++ Project/Main.cu
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE 16777216
+#define SIZE 1024
 #define TYPE double
 
 __global__ void gpuVectorAdd(TYPE* bufferIn1, TYPE* bufferIn2, TYPE* bufferOut, int bufferSize)
@@ -39,24 +39,17 @@ int main()
 		cpuBufferIn2[i] = (TYPE)rand() / (TYPE)RAND_MAX;
 	}
 
-	clock_t time0 = clock();
-
 	cudaMemcpy(gpuBufferIn1, cpuBufferIn1, SIZE * sizeof(TYPE), cudaMemcpyHostToDevice);
 	cudaMemcpy(gpuBufferIn2, cpuBufferIn2, SIZE * sizeof(TYPE), cudaMemcpyHostToDevice);
-
-	clock_t time1 = clock();
 
 	int threads = 64;
 	int blocks = (SIZE + threads - 1) / threads;
 
 	gpuVectorAdd<<<blocks, threads>>>(gpuBufferIn1, gpuBufferIn2, gpuBufferOut, SIZE);
+
 	cudaDeviceSynchronize();
 
-	clock_t time2 = clock();
-
 	cudaMemcpy(cpuBufferOut, gpuBufferOut, SIZE * sizeof(TYPE), cudaMemcpyDeviceToHost);
-
-	clock_t time3 = clock();
 
 	int errorCount = 0;
 
@@ -65,11 +58,8 @@ int main()
 		errorCount += ((cpuBufferIn1[i] + cpuBufferIn2[i]) != cpuBufferOut[i]);
 	}
 
-	printf("Elements:      %d\n", SIZE);
-	printf("Errors:        %d\n", errorCount);
-	printf("CPU-GPU Copy:  %.3f seconds\n", (double)(time1 - time0) / (double)CLOCKS_PER_SEC);
-	printf("GPU Execution: %.3f seconds\n", (double)(time2 - time1) / (double)CLOCKS_PER_SEC);
-	printf("GPU-CPU Copy:  %.3f seconds\n", (double)(time3 - time2) / (double)CLOCKS_PER_SEC);
+	printf("Elements: %d\n", SIZE);
+	printf("Errors:   %d\n", errorCount);
 
 	cudaFree(gpuBufferIn1);
 	cudaFree(gpuBufferIn2);
